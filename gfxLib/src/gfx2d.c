6219435bbbf012f3a2215d7fd2a6e5d6e31976b1
DamianPodrazaCode <damian.podraza@gmail.com>
2023-03-18 20:24:54 +0100

    modified:   gfxLib/src/gfx2d.c

---
+++ gfxLib/src/gfx2d.c
@@ -0,0 +1,6 @@
+#include "gfx2d.h"
+
+void gfx2dPoint(int x, int y) {}
+void gfx2dLine(int x0, int y0, int x1, int y1) {}
+void gfx2dCircle(int x, int y, int r) {}
+void gfx2dFillCircle(int x, int y, int r) {}
