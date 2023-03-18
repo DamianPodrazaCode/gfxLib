327394a9f5df10036fc36c8394ece4d64dad23fb
DamianPodrazaCode <damian.podraza@gmail.com>
2023-03-18 20:24:16 +0100

    modified:   gfxLib/src/hardwarePort.c

--- gfxLib/src/hardwarePort.c
+++ gfxLib/src/hardwarePort.c
@@ -1,6 +1,6 @@
 #include "hardwarePort.h"
 
-__weak void setPixel(unsigned short x, unsigned short y, unsigned short color) {}
-__weak void hLine(unsigned short x1, unsigned short x2, unsigned short y, unsigned short color) {}
-__weak void vLine(unsigned short x, unsigned short y1, unsigned short y2, unsigned short color) {} 
-__weak void copyBuffer(unsigned short* buffer, unsigned short x, unsigned short y, unsigned short heigh, unsigned short width) {}
+__weak void setPixel(int x, int y, unsigned short color) {}
+__weak void hLine(int x0, int x1, int y, unsigned short color) {}
+__weak void vLine(int x, int y0, int y1, unsigned short color) {}
+__weak void copyBuffer(unsigned short* buffer, int x, int y, int heigh, int width) {}
--- gfxLib/src/hardwarePort.h
+++ gfxLib/src/hardwarePort.h
@@ -5,9 +5,9 @@
 #define __weak   __attribute__((weak))
 #endif // __weak 
 
-void setPixel(unsigned short x, unsigned short y, unsigned short color);
-void hLine(unsigned short x1, unsigned short x2, unsigned short y, unsigned short color);
-void vLine(unsigned short x, unsigned short y1, unsigned short y2, unsigned short color);
-void copyBuffer(unsigned short* buffer, unsigned short x, unsigned short y, unsigned short heigh, unsigned short width);
+void setPixel(int x, int y, unsigned short color);
+void hLine(int x0, int x1, int y, unsigned short color);
+void vLine(int x, int y0, int y1, unsigned short color);
+void copyBuffer(unsigned short* buffer, int x, int y, int heigh, int width);
 
 #endif //HARDWARE_PORT_H
