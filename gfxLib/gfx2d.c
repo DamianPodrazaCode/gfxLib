cad03ee768e1553c7688005a02a877277b62e57e
DamianPodrazaCode <damian.podraza@gmail.com>
2023-03-24 08:18:28 +0100

    Signed-off-by: DamianPodrazaCode <damian.podraza@gmail.com>

--- gfxLib/gfx2d.c
+++ gfxLib/gfx2d.c
@@ -27,83 +27,37 @@ void gfx2d_line(int32_t x0, int32_t y0, int32_t x1, int32_t y1, uint16_t color) 
 //	}
 
 // 549
-//	register int x = x0;
-//	register int y = y0;
-//	register int e = 0;
-//	register int kx = (x <= x1) ? 1 : -1;
-//	register int ky = (y <= y1) ? 1 : -1;
-//	register int dx = ABS(x1 - x);
-//	register int dy = ABS(y1 - y);
-//
-//	if (dx >= dy) {
-//		e = dx >> 1;
-//		do {
-//			drawPixel(x, y, color);
-//			x = x + kx;
-//			e = e - dy;
-//			if (e < 0) {
-//				y = y + ky;
-//				e = e + dx;
-//			}
-//		} while (x != x1);
-//	} else {
-//		e = dy >> 1;
-//		do {
-//			drawPixel(x, y, color);
-//			y = y + ky;
-//			e = e - dx;
-//			if (e < 0) {
-//				x = x + kx;
-//				e = e + dy;
-//			}
-//		} while (y != y1);
-//	}
+	register int x = x0;
+	register int y = y0;
+	register int e = 0;
+	register int kx = (x <= x1) ? 1 : -1;
+	register int ky = (y <= y1) ? 1 : -1;
+	register int dx = ABS(x1 - x);
+	register int dy = ABS(y1 - y);
 
-//	register int dx = x1 - x0;
-//	register int dy = y1 - y0;
-//	register int stepI = dx / dy;
-//	register int stepM = dx % dy;
-//	register int e = dx;
-//
-//	do {
-//		drawPixel(x0, y0++, color);
-//		stepI = e / dy;
-//		stepM = e % dy;
-//		x0 = x0 + stepI;
-//		e = dx + stepM;
-//	} while (y0 != y1);
-
-//	register int x = x0;
-//	register int y = y0;
-//	register int e = 0;
-//	register int kx = (x <= x1) ? 1 : -1;
-//	register int ky = (y <= y1) ? 1 : -1;
-//	register int dx = ABS(x1 - x);
-//	register int dy = ABS(y1 - y);
-//
-//	if (dx >= dy) {
-//		e = dx >> 1;
-//		do {
-//			drawPixel(x, y, color);
-//			x = x + kx;
-//			e = e - dy;
-//			if (e < 0) {
-//				y = y + ky;
-//				e = e + dx;
-//			}
-//		} while (x != x1);
-//	} else {
-//		e = dy >> 1;
-//		do {
-//			drawPixel(x, y, color);
-//			y = y + ky;
-//			e = e - dx;
-//			if (e < 0) {
-//				x = x + kx;
-//				e = e + dy;
-//			}
-//		} while (y != y1);
-//	}
+	if (dx >= dy) {
+		e = dx >> 1;
+		do {
+			drawPixel(x, y, color);
+			x = x + kx;
+			e = e - dy;
+			if (e < 0) {
+				y = y + ky;
+				e = e + dx;
+			}
+		} while (x != x1);
+	} else {
+		e = dy >> 1;
+		do {
+			drawPixel(x, y, color);
+			y = y + ky;
+			e = e - dx;
+			if (e < 0) {
+				x = x + kx;
+				e = e + dy;
+			}
+		} while (y != y1);
+	}
 
 }
 
@@ -125,40 +79,48 @@ void gfx2d_ellipse(int32_t Xpos, int32_t Ypos, uint32_t XRadius, uint32_t YRadiu
 }
 
 typedef struct {
+	int32_t e;
+	int32_t kx;
+	int32_t ky;
 	int32_t dx;
-	int32_t sx;
 	int32_t dy;
-	int32_t sy;
-	int32_t error;
-	int32_t e2;
 	int32_t x;
 	int32_t y;
-	int32_t xTemp;
-	int32_t yTemp;
 } lineComplex_t;
 
-static void lineInit(lineComplex_t *line, int32_t *x0, int32_t *y0, int32_t *x1, int32_t *y1) {
+static inline void lineInit(lineComplex_t *line, int32_t *x0, int32_t *y0, int32_t *x1, int32_t *y1) {
+	line->e = 0;
+	line->kx = (*x0 <= *x1) ? 1 : -1;
+	line->ky = (*y0 <= *y1) ? 1 : -1;
 	line->dx = ABS(*x1 - *x0);
-	line->sx = (*x0 < *x1) ? 1 : -1;
-	line->dy = -ABS(*y1 - *y0);
-	line->sy = (*y0 < *y1) ? 1 : -1;
-	line->error = line->dx + line->dy;
-	line->e2 = 0;
-	line->xTemp = *x0;
-	line->yTemp = *y0;
+	line->dy = ABS(*y1 - *y0);
+	line->x = *x0;
+	line->y = *y0;
+
+	if (line->dx >= line->dy)
+		line->e = line->dx >> 1;
+	else
+		line->e = line->dy >> 1;
 }
 
-static void lineStep(lineComplex_t *line) {
-	line->x = line->xTemp;
-	line->y = line->yTemp;
-	line->e2 = line->error << 1;
-	if (line->e2 >= line->dy) {
-		line->error = line->error + line->dy;
-		line->xTemp = line->xTemp + line->sx;
-	}
-	if (line->e2 <= line->dx) {
-		line->error = line->error + line->dx;
-		line->yTemp = line->yTemp + line->sy;
+static inline void lineStep(lineComplex_t *line) {
+	if (line->dx >= line->dy) {
+		do {
+			line->x += line->kx;
+			line->e -= line->dy;
+			if (line->e < 0) {
+				line->y += line->ky;
+				line->e += line->dx;
+				break;
+			}
+		} while (1);
+	} else {
+		line->y += line->ky;
+		line->e -= line->dx;
+		if (line->e < 0) {
+			line->x += line->kx;
+			line->e += line->dy;
+		}
 	}
 }
 
@@ -178,30 +140,28 @@ void gfx2d_fillTriangle(int32_t x0, int32_t y0, int32_t x1, int32_t y1, int32_t 
 		SWAP(x1, x2);
 	}
 
-	int x3 = (int) (x0 + ((float) (y1 - y0) / (float) (y2 - y0)) * (x2 - x0));
-	int y3 = y1;
-
-	//gfx2d_line(x0, y0, x1, y1, 0x07FF);
-
 	lineComplex_t line1, line2, line3;
 
 	lineInit(&line1, &x0, &y0, &x1, &y1);
 	lineInit(&line2, &x0, &y0, &x2, &y2);
-	while ((line1.x != x1) || (line1.y != y1)) {
+
+	for (int i = y0; i < y1; i++) {
+//		drawPixel(line1.x, line1.y, color);
+//		drawPixel(line2.x, line2.y, color);
+		drawHLine(line1.x, line2.x, line1.y, color);
 		lineStep(&line1);
-		drawPixel(line1.x, line1.y, color);
-		if (line2.y <= line1.y) {
-			lineStep(&line2);
-			drawPixel(line2.x, line2.y, color);
-		}
+		lineStep(&line2);
 	}
 
-//		gfx2d_line(x0, y0, x1, y1, 0x07FF);
-//		gfx2d_line(x0, y0, x3, y3, 0x07FF);
-//		gfx2d_line(x1, y1, x3, y3, 0x001F);
-//
-//		gfx2d_line(x2, y2, x1, y1, 0x07FF);
-//		gfx2d_line(x2, y2, x3, y3, 0x07FF);
+	lineInit(&line3, &x1, &y1, &x2, &y2);
+
+	for (int i = y1; i < y2; i++) {
+//		drawPixel(line2.x, line2.y, color);
+//		drawPixel(line3.x, line3.y, color);
+		drawHLine(line3.x, line2.x, line3.y, color);
+		lineStep(&line2);
+		lineStep(&line3);
+	}
 
 //	int16_t t1x, t2x, y, minx, maxx, t1xp, t2xp;
 //	uint8_t changed1 = 0;
